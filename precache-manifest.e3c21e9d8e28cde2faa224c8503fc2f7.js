a3f8b12173008451c24e7ef9764efd694a701f13
mmannella <MMANNELLA@santandertecnologia.com.ar>
2020-08-29 19:54:18 -0300

    Updates

---
+++ precache-manifest.e3c21e9d8e28cde2faa224c8503fc2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b180773c2ee7d39668e530b42fca6807",
+    "url": "/Parseador/index.html"
+  },
+  {
+    "revision": "299746799d9f5f7cafe8",
+    "url": "/Parseador/static/css/main.bc11963f.chunk.css"
+  },
+  {
+    "revision": "82611fb1846ea77b8db6",
+    "url": "/Parseador/static/js/2.2e453209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Parseador/static/js/2.2e453209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299746799d9f5f7cafe8",
+    "url": "/Parseador/static/js/main.1ad01831.chunk.js"
+  },
+  {
+    "revision": "83e832a8a1e903a52f47",
+    "url": "/Parseador/static/js/runtime-main.efa04883.js"
+  }
+]);
